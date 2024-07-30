f71e1de55b95aca67ec02293dac91e5bf30cea2f
T-H-Badhon <thbadhons@gmail.com>
2024-07-30 12:09:57 +0600

    computer navber done

--- src/components/shared/nav/NavMenu.tsx
+++ src/components/shared/nav/NavMenu.tsx
@@ -6,24 +6,33 @@ import { HiPlay } from "react-icons/hi2";
 const NavMenu = () => {
   return (
     <div className="flex space-x-3 items-center">
-      <div className="group hover:bg-secondaryHover py-1 px-4 rounded-s-full rounded-e-full h-8 overflow-hidden">
+      <Link
+        href={"/"}
+        className="group h-8 overflow-hidden text-base hover:bg-secondaryHover py-1 px-4 rounded-s-full rounded-e-full"
+      >
         <h1 className="group-hover:-translate-y-8 transition-transform duration-300">
           Home
         </h1>
         <h1 className="group-hover:-translate-y-6 transition-transform duration-300">
           Home
         </h1>
-      </div>
-      <div className="group hover:bg-secondaryHover py-1 px-4 rounded-s-full rounded-e-full h-8 overflow-hidden">
+      </Link>
+      <Link
+        href={"/about"}
+        className="group h-8 overflow-hidden text-base hover:bg-secondaryHover py-1 px-4 rounded-s-full rounded-e-full"
+      >
         <h1 className="group-hover:-translate-y-8 transition-transform duration-300">
           About Us
         </h1>
         <h1 className="group-hover:-translate-y-6 transition-transform duration-300">
           About Us
         </h1>
-      </div>
+      </Link>
       <div className="relative group">
-        <div className="group-hover:bg-secondaryHover flex items-baseline space-x-2 py-1 px-4 rounded-s-full rounded-e-full h-8 overflow-hidden">
+        <Link
+          href={"/products"}
+          className="h-8 overflow-hidden text-base flex items-baseline group-hover:bg-secondaryHover space-x-2 py-1 px-4 rounded-s-full rounded-e-full"
+        >
           <div>
             <h1 className="group-hover:-translate-y-8 transition-transform duration-300">
               Products
@@ -32,30 +41,41 @@ const NavMenu = () => {
               Products
             </h1>
           </div>
-          <HiPlay className="relative top-[2px] rotate-45 w-2 h-2 "/>
-        </div>
+          <HiPlay className="relative top-[2px] rotate-45 w-2 h-2  " />
+        </Link>
 
-        <div className="hidden absolute left-1/2 z-10 group-hover:flex w-screen max-w-max -translate-x-1/2 px-4 transition data-[closed]:translate-y-1 data-[closed]:opacity-0 data-[enter]:duration-200 data-[leave]:duration-150 data-[enter]:ease-out data-[leave]:ease-in">
+        <div className="hidden group-hover:flex h-0 group-hover:h-fit absolute left-1/2 z-10   w-screen max-w-max -translate-x-1/2 px-4 transition-all ease-in-out delay-1000 duration-1000">
           <div className="w-[250px] flex-auto overflow-hidden rounded-3xl mt-4 bg-black text-white text-base leading-6 shadow-lg ring-1 ring-gray-900/5">
             <div className="p-8 flex flex-col space-y-4 ">
-              {productList.map((product) => (
-                <Link key={product.name} href="#" className="hover:text-red-400">
-                  {product.name}
-                </Link>
-              ))}
+              {productList.map((product) => {
+                
+                return(
+                  <Link
+                    key={product.name}
+                    href="#"
+                    className="group/inner flex items-center hover:text-red-400 transition duration-300"
+                  >
+                    <HiPlay className="hidden group-hover/inner:block relative top-[5px] rotate-45 w-2 h-2" />
+                    <h1 className="transition-transform  duration-500 group-hover/inner:translate-x-3">{product.name}</h1>
+                  </Link>
+                )
+              })}
             </div>
           </div>
         </div>
       </div>
-      <div className="group hover:bg-secondaryHover py-1 px-4 rounded-s-full rounded-e-full h-8 overflow-hidden">
+      <Link
+        href={"/events"}
+        className="group h-8 overflow-hidden text-base hover:bg-secondaryHover py-1 px-4 rounded-s-full rounded-e-full "
+      >
         <h1 className="group-hover:-translate-y-8 transition-transform duration-300">
           Events
         </h1>
         <h1 className="group-hover:-translate-y-6 transition-transform duration-300">
           Events
         </h1>
-      </div>
-      <div className="group hover:bg-secondaryHover py-1 px-4 rounded-s-full rounded-e-full h-8 overflow-hidden">
+      </Link>
+      <div className="group h-8 overflow-hidden text-base hover:bg-secondaryHover py-1 px-4 rounded-s-full rounded-e-full ">
         <h1 className="group-hover:-translate-y-8 transition-transform duration-300">
           Catalog
         </h1>
@@ -63,14 +83,17 @@ const NavMenu = () => {
           Catalog
         </h1>
       </div>
-      <div className="group hover:bg-secondaryHover py-1 px-4 rounded-s-full rounded-e-full h-8 overflow-hidden">
+      <Link
+        href={"/contact"}
+        className="group h-8 overflow-hidden text-base hover:bg-secondaryHover py-1 px-4 rounded-s-full rounded-e-full "
+      >
         <h1 className="group-hover:-translate-y-8 transition-transform duration-300">
           Contact Us
         </h1>
         <h1 className="group-hover:-translate-y-6 transition-transform duration-300">
           Contact Us
         </h1>
-      </div>
+      </Link>
     </div>
   );
 };
--- src/components/shared/nav/Navbar.tsx
+++ src/components/shared/nav/Navbar.tsx
@@ -1,27 +1,34 @@
-import Image from 'next/image';
-import React from 'react';
-import { HiMagnifyingGlass } from 'react-icons/hi2';
-import NavMenu from './NavMenu';
+import Image from "next/image";
+import React from "react";
+import { HiMagnifyingGlass } from "react-icons/hi2";
+import NavMenu from "./NavMenu";
 
 const Navbar = () => {
-    return (
-        <div className=' flex justify-between items-center bg-white mx-8 my-5  px-8 rounded-2xl '>
-            <div >
-                <Image src={"https://nbgnl.com/wp-content/uploads/2019/10/gnl-logo.png"} width={100} height={100} alt="logo" />
-            </div>
-            <div>
-                <NavMenu/>
-            </div>
-            <div className='flex items-center space-x-5'>
-                <div>
-                    <HiMagnifyingGlass className=' text-2xl'/>
-                </div>
-                <div className='py-4 px-10 bg-red-300  border rounded-s-full rounded-e-full'>
-                    Get in touch
-                </div>
-            </div>
+  return (
+    <div className=" flex justify-between items-center bg-white mx-8 my-5  px-8 rounded-2xl ">
+      <div>
+        <Image
+          src={"https://nbgnl.com/wp-content/uploads/2019/10/gnl-logo.png"}
+          width={100}
+          height={100}
+          alt="logo"
+        />
+      </div>
+      <div>
+        <NavMenu />
+      </div>
+      <div className="flex items-center space-x-5">
+        <div>
+          <HiMagnifyingGlass className=" text-2xl" />
         </div>
-    );
+        <div className="p-[2px] bg-gradient-to-r from-blue-800 to-red-600 rounded-s-full rounded-e-full">
+          <div className="py-4 px-10 bg-white hover:bg-gradient-to-r from-blue-800 to-red-600 text-base hover:text-white  rounded-s-full rounded-e-full">
+            Get in touch
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
